bb78f93a45dcf2bde70e4a49beb4793b059848d0
Veronika Kreinina <167687339+Veronika-Kreinina@users.noreply.github.com>
2025-05-16 13:04:42 +0200

    initial commit

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,3 +2,8 @@ export const errorHandler = (error, req, res, next) => {
   const { status = 500, message } = error;
   res.status(status).json({ status, message });
 };
+
+export const error = (error, req, res, next) => {
+  const { status = 500, message } = error;
+  res.status(status).json({ status, message });
+};
